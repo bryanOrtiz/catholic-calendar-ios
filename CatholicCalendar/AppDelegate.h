4d2676fa19b0d4f09bdaa3b63bf1ed751b7ea27d
Bryan Ortiz <ortiz.b.t@gmail.com>
2017-03-12 19:32:12 -0500

    Initial Commit

---
+++ CatholicCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CatholicCalendar
+//
+//  Created by Bryan Ortiz on 3/12/17.
+//  Copyright © 2017 Ortiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CatholicCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CatholicCalendar
+//
+//  Created by Bryan Ortiz on 3/12/17.
+//  Copyright © 2017 Ortiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
